2abcd8f9320abe74e5ce53967c161475e9d5f05d
dakeyama <dakeyama@umebius.com>
2019-08-01 01:54:52 +0900

    トップのバナーを差し替え

--- Event.php
+++ Event.php
@@ -2,15 +2,42 @@
 
 namespace Plugin\BannerManagement4;
 
+use Eccube\Event\TemplateEvent;
+use Plugin\BannerManagement4\Repository\BannerRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class Event implements EventSubscriberInterface
 {
+
+    /**
+     * @var BannerRepository
+     */
+    protected $bannerRepository;
+
+    /**
+     * コンストラクタ
+     *
+     * @param BannerRepository $bannerRepository
+     */
+    public function __construct(
+        BannerRepository $bannerRepository
+    ) {
+        $this->bannerRepository = $bannerRepository;
+    }
+
     /**
      * @return array
      */
     public static function getSubscribedEvents()
     {
-        return [];
+        return [
+            'index.twig' => ['onIndexTwig',]
+        ];
+    }
+
+
+    public function onIndexTwig(TemplateEvent $event) {
+        $event->setParameter('TopBanners', $this->bannerRepository->getBanners(1));
+        $event->addSnippet('@BannerManagement4/index_slider.twig');
     }
 }
